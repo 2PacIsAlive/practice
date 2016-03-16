f098217099fac712e4462023003300624633bf11
Jared Jolton <jared.jolton@gmail.com>
2016-03-16 01:45:11 -0600

    added k-means clustering visualization

---
+++ python_KB/datamining/k_means.py
@@ -0,0 +1,62 @@
+#!usr/bin/env python
+
+import random
+from pyprocessing  import *
+from scipy.spatial import distance
+
+
+class KMeansCluster:
+
+    #grid = [x for x in range(50,275)] + [x for x in range(325,550)]
+    grid = [x for x in range(50,550)]
+    
+    iteration = 0
+
+    def __init__(self, num_centroids, num_objs):
+        self.centroids = [{'x': random.choice(self.grid),        
+                           'y': random.choice(self.grid), 
+                           'r': random.randint(0,255),
+                           'g': random.randint(0,255),
+                           'b': random.randint(0,255),
+                           'class': x }  for x in range(num_centroids)]
+        self.objects   = [{'x': random.choice(self.grid), 
+                           'y': random.choice(self.grid), 
+                           'r': random.randint(0,255),
+                           'g': random.randint(0,255),
+                           'b': random.randint(0,255),
+                           'class': None }  for x in range(num_objs)]
+
+    def find_class(self, point, centroids):
+        dst = [(distance.euclidean((point['x'],point['y']), (x['x'],x['y'])), x) for x in centroids]
+        return min(dst, key = lambda x: x[0])
+
+    def update(self):
+        for point in self.objects:
+            new_class = self.find_class(point, self.centroids)[1] 
+            point['r'] = new_class['r']
+            point['g'] = new_class['g']
+            point['b'] = new_class['b']
+            point['class'] = new_class['class']
+
+    def recompute_centroids(self):
+        for centroid in self.centroids:
+            group = [x for x in self.objects if x['class'] == centroid['class']]
+            centroid['x'] = sum([x['x'] for x in group]) / len([x['x'] for x in group])
+            centroid['y'] = sum([x['y'] for x in group]) / len([x['y'] for x in group])
+
+def setup():
+    size(600,600)
+    noStroke()
+
+def draw():
+    background(50)
+    for point in cluster.objects:
+        fill(point['r'], point['g'], point['b'])
+        rect(point['x'], point['y'], 10, 10)
+    cluster.update()
+    cluster.recompute_centroids()
+    save('images/'+str(cluster.iteration+83)+'.png')
+    cluster.iteration += 1
+
+cluster = KMeansCluster(input("Clusters: "), input("Objects: "))
+run() # for fun
--- python_KB/random_challenges/palindrome_checker.py
+++ python_KB/random_challenges/palindrome_checker.py
@@ -1,5 +1,7 @@
 #!usr/bin/env python
 
+import itertools
+
 def checkPalindrome(word):
 	if len(word) < 2:
 		return 
@@ -18,8 +20,11 @@ def loadWords(file_):
 def main():
 	data = loadWords("assets/lowercase.txt")
 	for word in data:
-		if checkPalindrome(word) == None: 
-			print word, "is a palindrome!"
+		print word
+		permutations = list(map("".join, itertools.permutations(word)))
+		for combo in permutations:
+			if checkPalindrome(combo) == None: 
+				print combo, "is a palindrome!", "ROOT WORD:", word
 		else:
 			#print word, "is not a palindrome."
 			pass
